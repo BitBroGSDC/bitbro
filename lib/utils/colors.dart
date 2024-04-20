de8b2ef1cb49bc859be5cc42c67cae4226ecefc0
DiegoZaff <diego.zaffe@gmail.com>
2024-04-20 18:08:31 +0200

    moved router and colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+
+
+
+import 'package:flutter/material.dart';
+
+const bluScuro = Color(0xFF33404F);
+const bianco = Colors.white;
+const verdeBrillante = Color(0xFF00DDA3);
+const rosaErrore = Color(0xFFEC9696);
