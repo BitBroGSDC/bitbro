212f21fd1308e2bc7d85d88b0208e5a659990068
Tommaso Morganti <tommaso.morganti01@gmail.com>
2024-04-20 23:01:18 +0200

    added graph tap registration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:bitbro/components/home/homegraph.dart';
 import 'package:bitbro/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:ionicons/ionicons.dart';
 
 import '../components/home/CurrentDayQuestion.dart';
 
@@ -26,34 +25,17 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: bluScuro,
       body: Stack(children: [
-        Column(
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: const Row(
-                children: [
-                  Icon(
-                    Ionicons.refresh_circle,
-                    color: Colors.white,
-                  ),
-                  Text('Profile', style: TextStyle(color: Colors.white))
-                ],
-              ),
-            ),
-            HomeGraph(
-              top: 'Tizio',
-              bottom: 'Caio',
-              you: 'Sempronio',
-              topData: const [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-              bottomData: const [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-              youData: const [5, 6, 7, 8, 9, 10, 9, 8, 7, 6],
-              navigateToFullScoreboard: () {
-                print("ciao");
-              },
-            ),
-          ],
+        HomeGraph(
+          top: 'Tizio',
+          bottom: 'Caio',
+          you: 'Sempronio',
+          topData: const [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          bottomData: const [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+          youData: const [5, 6, 7, 8, 9, 10, 9, 8, 7, 6],
+          navigateToFullScoreboard: () {
+            print("ciao");
+          },
         ),
-        /* WidgetTommi(), */
         DraggableScrollableSheet(
           controller: controller,
           initialChildSize: 0.5,
